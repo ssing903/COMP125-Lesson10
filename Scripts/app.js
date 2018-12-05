347789520e7fad8da7fa40095e8c3527cfa05f57
learner <35473820+ssing903@users.noreply.github.com>
2018-12-05 18:31:55 -0500

    Update app.js

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,6 +1,9 @@
 // core module - IIFE
 (function() {
   // App variables
+
+  // STEP 1 : create xhr variable 
+
   let XHR;
 
 
@@ -12,6 +15,8 @@
       `%c App Initializing...`,
       "font-weight: bold; font-size: 20px;"
     );
+
+    // step - instantiate an xmlhttprequest 
     XHR = new XMLHttpRequest();
     Main();
   }
@@ -22,9 +27,21 @@
    */
   function Main() {
     console.log(`%c App Started...`, "font-weight: bold; font-size: 20px;");
-    XHR.addEventListener("readystatechange",function(){
-
-    })
+    
+     // STEP 3 - Set up an event listner / handler that
+    // listens for a readystatechange and requires
+    // the readyState code to be "4" and the status to be "200"
+    XHR.addEventListener("readystatechange", function(){
+      if((XHR.readyState === 4) && (XHR.status === 200)) {
+        // responseText is the data we are recieving from the server
+        console.log(XHR.responseText);
+      }
+    });
+     // STEP 4 - use the open method of the XHR object to send a GET request
+    // you need to send the URL information
+    XHR.open("GET", "/Views/partials/header.html");
+     // STEP 5 - complete the request with the send method
+    XHR.send();
   }
 
   window.addEventListener("load", Start);
